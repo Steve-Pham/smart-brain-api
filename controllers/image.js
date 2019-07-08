c4566ef2189fec479ecdd062ba2ff10741e246af
Steve Pham <stevepham@Steves-MBP.cgocable.net>
2019-07-08 14:45:05 -0400

    first commit server

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '2c797d4a634a408eafeb9444f5cdcaca'
 });
 
 const handleApiCall = (req, res) => {
---
+++ postgres/deploy_schemas.sql
@@ -0,0 +1,7 @@
+-- Deploy fresh database tables
+
+\i '/docker-entrypoint-initdb.d/tables/users.sql'
+\i '/docker-entrypoint-initdb.d/tables/login.sql'
+
+
+
